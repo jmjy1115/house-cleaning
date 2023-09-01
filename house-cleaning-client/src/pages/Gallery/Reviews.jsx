5b98f5972617546c52d6058311a396d8f62c29c9
jmjy1115 <108736255+jmjy1115@users.noreply.github.com>
2023-09-01 22:22:38 +0900

    Implement review board using state hooks and lifecycle methods
    This commit introduces a board that displays the review data sent by the server. Leveraging state hooks and lifecycle methods, we successfully render the data in a board format.
    
    Upcoming Work:
    - Display the current page number at the bottom of the review board.
    - Implement server-side response code for review data.
    - Add functionalities for creating, updating, and deleting reviews.
    - Ensure that when posting a review, user details are captured for future authorization regarding modifications or deletions.
    - Finalize the review board's UI/UX.

--- house-cleaning-client/src/pages/Gallery/Reviews.jsx
+++ house-cleaning-client/src/pages/Gallery/Reviews.jsx
@@ -1,8 +1,43 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './ProjectCases.css';
+  
 function Reviews() {
+  const [ reviews, setReviews ] = useState([]);
+  const [ currentPage, setCurrentPage ] = useState(1);
+  const [ totalPages, setTotalPages ] = useState(1);
+  
+  // 생명주기를 활용하면 currentPage가 변경될 때마다 게시판이 최신화 됨
+  // 버튼 클릭으로 currentPage는 자동으로 변경됨
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_SERVER_URL}/review?page=${currentPage}&limit=10`
+        );
+        setReviews(response.data.data);
+        setTotalPages(response.data.totalPages);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    loadData();
+  }, [currentPage]);
+
   return (
-    <div></div>
+    <div className="review-board">
+      <ul>
+        {reviews.map(review => (
+          <li key={review.id}>{review.title}</li>
+        ))}
+        {reviews.length < 10 &&
+          [...Array.from({ length: 10 - reviews.length })].map((_, index) => (
+            <li key={`empty-${index}`}></li>
+          ))
+        }
+      </ul>
+    </div>
   );
 }
 
